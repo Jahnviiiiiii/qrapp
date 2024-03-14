be4ec12212536211622f194910840f3c48f63ea1
jahnvi <143591147+Jahnviiiiiii@users.noreply.github.com>
2024-03-14 17:03:04 +0530

    latest

--- lib/view_page.dart
+++ lib/view_page.dart
@@ -17,9 +17,37 @@ class _ViewPageState extends State<ViewPage> {
           children: [
           Image.network("https://cdn2.iconfinder.com/data/icons/outlined-set-1/29/people-512.png",height: 100, width: 150,),
         SizedBox(height:50,),
+            RichText(
+              text: TextSpan(
+                  style: TextStyle(fontSize: 22, color: Colors.black),
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: 'Name',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ]
+              ),
+            ),
+            RichText(
+              text: TextSpan(
+                  style: TextStyle(fontSize: 22, color: Colors.black),
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: 'flutter developer',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ]
+              ),
+            ),
 
-          Text('name'),
-          Text('flutter developer'),
 
         ],
       ),
